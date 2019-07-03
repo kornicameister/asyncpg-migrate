3057c37147d333a23ddf580ae31b4ce77063ec7a
Tomasz Trębski <kornicameister@gmail.com>
2019-07-03 23:39:23 +0200

    Tryigng to make it connect

--- tests/integration/test_migration_helper.py
+++ tests/integration/test_migration_helper.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 import pytest_mock as ptm
 
@@ -8,9 +10,15 @@ from asyncpg_migrate.cmd import _helper
 
 @pytest.mark.asyncio
 async def test_ensure_migrations_table_create(mocker: ptm.MockFixture) -> None:
+    db_user = os.getenv('POSTGRES_USER')
+    db_password = os.getenv('POSTGRES_PASSWORD')
+    db_name = os.getenv('POSTGRES_DB')
+    db_port = os.getenv('POSTGRES_PORT')
+
     config = model.Config(
         script_location=mocker.stub(),
-        database_dsn='postgres://root:root@localhost:5432/asyncpg_migrate',
+        database_dsn=f'postgres://{db_user}:{db_password}@localhost:{db_port}/{db_name}',
         database_name='asyncpg_migrate',
     )
+
     await _helper.migrations_table_create(config)
