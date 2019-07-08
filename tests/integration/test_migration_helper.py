3c8d2bd5bb43c6c914e268733ff92098adeba394
Tomasz Trębski <kornicameister@gmail.com>
2019-07-08 23:53:19 +0200

    Final fixes to pipeline

--- tests/integration/test_migration_helper.py
+++ tests/integration/test_migration_helper.py
@@ -1,5 +1,6 @@
 import os
 
+import asyncpg
 import pytest
 import pytest_mock as ptm
 
@@ -8,18 +9,39 @@ from asyncpg_migrate import model
 from asyncpg_migrate.commands import _helper
 
 
-@pytest.mark.asyncio
-async def test_ensure_migrations_table_create(mocker: ptm.MockFixture) -> None:
+@pytest.fixture(scope='session')
+def db_name() -> str:
+    db_name = os.getenv('POSTGRES_DB')
+    assert db_name, 'db_name not set'
+    return db_name
+
+
+@pytest.fixture(scope='session')
+def db_dsn(db_name: str) -> str:
     db_user = os.getenv('POSTGRES_USER')
     db_password = os.getenv('POSTGRES_PASSWORD')
-    db_name = os.getenv('POSTGRES_DB')
     db_port = os.getenv('POSTGRES_PORT')
     db_host = os.getenv('POSTGRES_HOST')
 
+    assert db_user, 'db_user not set'
+    assert db_password, 'db_password not set'
+    assert db_port, 'db_port not set'
+    assert db_host, 'db_host not set'
+
+    return f'postgres://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+
+
+@pytest.mark.asyncio
+async def test_ensure_migrations_table_create(
+        db_dsn: str,
+        db_name: str,
+        mocker: ptm.MockFixture,
+) -> None:
+
     config = model.Config(
         script_location=mocker.stub(),
-        database_dsn=f'postgres://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}',
-        database_name='asyncpg_migrate',
+        database_dsn=db_dsn,
+        database_name=db_name,
     )
 
     await _helper.migrations_table_create(
@@ -27,3 +49,14 @@ async def test_ensure_migrations_table_create(mocker: ptm.MockFixture) -> None:
         migrations_table_schema=constants.MIGRATIONS_SCHEMA,
         migrations_table_name=constants.MIGRATIONS_TABLE,
     )
+
+    con = await asyncpg.connect(dsn=db_dsn)
+    table_name_in_db = await con.fetchval(
+        """
+        select to_regclass('{schema}.{table}')
+        """.format(
+            schema=constants.MIGRATIONS_SCHEMA,
+            table=constants.MIGRATIONS_TABLE,
+        ),
+    )
+    assert table_name_in_db == constants.MIGRATIONS_TABLE
