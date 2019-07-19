85ef7bf29e92e8495804137cf1212371167bb4fa
Tomasz Trębski <kornicameister@gmail.com>
2019-07-19 22:20:43 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-from pathlib import Path
 import sys
-import typing as t
 
 import setuptools
 
@@ -13,38 +11,8 @@ __url__ = 'https://github.com/kornicameister/asyncpg-migrate'
 if sys.version_info < (3, 6):
     raise RuntimeError('asyncpg-migrate requires Python 3.6 or greater')
 
-
-def read_requirements(
-        path: Path,
-        all_requirements: t.Optional[t.List[str]] = None,
-) -> t.List[str]:
-    if not all_requirements:
-        all_requirements = []
-    with path.open('r') as handler:
-        r_l = handler.readline()
-        while r_l:
-            r_l = r_l.replace('\n', '').strip()
-            if r_l.startswith('-r'):
-                nested_file_path = Path(r_l.split('-r ')[1])
-                if nested_file_path.is_absolute():
-                    all_requirements.extend(read_requirements(nested_file_path))
-                else:
-                    all_requirements.extend(
-                        read_requirements(
-                            Path.cwd() / nested_file_path,
-                        ),
-                    )
-            elif r_l:
-                all_requirements.append(r_l)
-            r_l = handler.readline()
-    return all_requirements
-
-
 setuptools.setup(
     setup_requires='setupmeta',
     # setupmeta options
     versioning='post',
-    # custom overrides
-    install_requires=read_requirements(Path('./requirements.txt')),
-    tests_require=read_requirements(Path('./test-requirements.txt')),
 )
